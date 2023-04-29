0c5ac57929bcdbc73b0577d332a9f2bc85670315
Alaa Elghamry <alaa.elghamry7@gmail.com>
2023-04-29 19:33:12 +0300

    save expenses

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css'
 
-const ExpenseForm  = () =>{
-    const titleChangeHandler =(e)=>{
-   console.log(e.target.value);
-    }
- return (
-     <form>
- <div className="new-expense__controls">
-    <div className="new-expense__control">
-        <label>Title</label>
-        <input type='text' onChange={titleChangeHandler} />
-    </div>  
-    <div className="new-expense__control">
-        <label>Amount</label>
-        <input type='number' min='0.01' step='0.01' />
-    </div>  
-    <div className="new-expense__control">
-        <label>Date</label>
-        <input type='Date' />
-    </div>  
- </div>
- <div className="new-expense__actions">
-  <button type="submit">Add Expense</button>
- </div>
- </form>
+const ExpenseForm = () => {
+    // const [enteredTitle, setenteredTitle] = useState('')
+    // const [enteredAmount, setenteredAmount] = useState('')
+    // const [enteredDate, setenteredDate] = useState('')
+    const [userInput, setUserInput] =  useState({
+        enteredTitle:'',   
+        enteredAmount:'',   
+        enteredDate:'',   
+    })
+     
 
- );
+    const titleChangeHandler = (e) => {
+        // setenteredTitle(e.target.value);
+        setUserInput((prevState) => {
+
+      return { ...prevState, enteredTitle: e.target.value };
+    });
+    }
+
+    const amountChangeHandler = (e) => {
+        setUserInput((prevState) => {
+            return { ...prevState, enteredAmount: e.target.value };
+          });
+    }
+
+    const dateChangeHandler = (e) => {
+        setUserInput((prevState) => {
+            return { ...prevState, enteredDate: e.target.value };
+          });
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    
+        const expenseData = {
+          title: userInput.enteredTitle,
+          amount: userInput.enteredAmount,
+          date: new Date(userInput.enteredDate),
+        };
+  console.log(expenseData);
+
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type='text' onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type='Date' onChange={dateChangeHandler} />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+
+    );
 }
 
 export default ExpenseForm
